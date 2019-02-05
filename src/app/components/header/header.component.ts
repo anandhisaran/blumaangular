7f8aea48b34da74663b3bb16eecbef771beff750
sysnx <anandhi@systimanx.com>
2019-02-05 17:21:14 +0530

    header menu in mobile view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,7 +11,57 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     $(document).ready(function() {
-
+     
+      document.addEventListener('DOMContentLoaded', function () {
+        var dropdown = document.querySelector('.dropdown');
+        dropdown.addEventListener('click', function(event) {
+          event.stopPropagation();
+          dropdown.classList.toggle('is-active');
+        });
+        // Get all "navbar-burger" elements
+        var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+      
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+      
+          // Add a click event on each of them
+          $navbarBurgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+      
+              // Get the target from the "data-target" attribute
+              var target = $el.dataset.target;
+              var $target = document.getElementById(target);
+      
+              // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+              $el.classList.toggle('is-active');
+              $target.classList.toggle('is-active');
+      
+            });
+          });
+        }
+      
+      });
+      // Close mobile & tablet menu on item click
+       $('.navbar-item').each(function(e) { 
+         $(this).click(function(){ 
+           if($('#navbar-burger-id').hasClass('is-active')){ 
+             $('#navbar-burger-id').removeClass('is-active');
+              $('#navbar-menu-id').removeClass('is-active'); } }); }); 
+              // Open or Close mobile & tablet menu 
+              $('#navbar-burger-id').click(function () { if($('#navbar-burger-id').hasClass('is-active'))
+              { $('#navbar-burger-id').removeClass('is-active'); $('#navbar-menu-id').removeClass('is-active'); }
+              else { $('#navbar-burger-id').addClass('is-active'); $('#navbar-menu-id').addClass('is-active'); } });
+      // var dropdown = document.querySelector('.dropdown');
+      // dropdown.addEventListener('click', function(event) {
+      //   event.stopPropagation();
+      //   dropdown.classList.toggle('is-active');
+      // });
+       
+      // var dropdown = document.querySelector('.dropdown');
+      // dropdown.addEventListener('click', function(event) {
+      //   event.stopPropagation();
+      //   dropdown.classList.toggle('is-active');
+      // });
       // Check for click events on the navbar burger icon
       $(".navbar-burger").click(function() {
     
@@ -20,6 +70,13 @@ export class HeaderComponent implements OnInit {
           $(".navbar-menu").toggleClass("is-active");
     
       });
+      $("#navbarExampleTransparentExample").click(function() {
+    
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+       $(".navbar-dropdown").toggleClass("is-active");
+        $(".navbar-dropdown").removeClass("is-hidden-mobile");
+  
+    });
     });
   }
 
